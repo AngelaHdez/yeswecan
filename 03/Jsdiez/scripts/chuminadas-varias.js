572b90caf6dd61675be6550920e5d42a5c9bad1a
Jesús S.D. <jesus.silla@miraiespana.com>
2013-05-31 11:10:11 +0200

    HTML5 form
    WIP

---
+++ 03/Jsdiez/scripts/chuminadas-varias.js
@@ -0,0 +1,16 @@
+﻿jQuery(document).ready(function(){
+	jQuery('#header').delay(200).animate({
+		opacity: 1,
+		top: '50%'
+	}, 1000);
+	
+	jQuery('.call_form').click(function(){
+		jQuery('.call_form').fadeOut('slow');
+		jQuery('#header').addClass('visible-form');
+		jQuery('#header').animate({
+		}, 1000, function() {
+			jQuery('#content').fadeIn('slow');	
+		})
+	});
+
+})
