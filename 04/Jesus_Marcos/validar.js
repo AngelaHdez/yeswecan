061ab5f30bf209007ef390fb6e22470476d79ec7
Jesús Marcos <jesusmarcosridao@gmail.com>
2013-06-04 16:50:40 +0200

    Formulario y validación en HTML + Bolas extras (sin estilos)
    Sin estilos

---
+++ 04/Jesus_Marcos/validar.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(function() {
+
+	var form = $('#formOne');
+	var inputNombre = $("input#nombre");
+	var inputApellidos = $("input#apellidos");
+	var inputPoblacion = $("input#poblacion");
+	var enviar = $("input#button");
+	
+	 form.submit(function(e){
+	 
+		$("p.poblacionError").empty();
+		$("p.apellidosError").empty();
+		$("p.nombreError").empty();
+		
+		if (inputNombre.val() == "")
+		{
+			e.preventDefault();
+			$("p.nombreError").append("El nombre es obligatorio");
+		}
+		if (inputApellidos.val() == "")
+		{
+			e.preventDefault();
+			$("p.apellidosError").append("Los apellidos son obligatorios");
+		}
+		if (inputPoblacion.val() == "")
+		{
+			e.preventDefault();
+			$("p.poblacionError").append("La población es obligatoria");
+		}
+	});
+});
+
