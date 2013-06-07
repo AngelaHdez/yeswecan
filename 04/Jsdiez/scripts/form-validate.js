f8af2e968e82059ec9070542e1c0f1ef0fa1f8df
Jesús S.D. <jesus.silla@miraiespana.com>
2013-06-07 10:12:31 +0200

    Detect if some input is empty and add some styles

--- 04/Jsdiez/scripts/form-validate.js
+++ 04/Jsdiez/scripts/form-validate.js
@@ -1,3 +1,22 @@
 ﻿var inputForm = document.getElementsByTagName('input');
 var sendForm = document.getElementsByClassName('send_form');
 var form = document.fake;
+
+
+
+//capturo evento submit
+function callFormValidate()
+{
+  formValidate();
+  return false;
+}
+
+//reviso que los campos no esten vacios
+formValidate = function(){
+	for (i =0; i < inputForm.length;i++) {
+		if(inputForm[i].value == "") {
+			alert('hay un campo vacio')
+			return false;
+		}
+	}
+}
