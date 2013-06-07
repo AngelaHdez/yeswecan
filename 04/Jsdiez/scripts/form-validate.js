b7a3941ee5be73f6fdc1353b35c08d92ccfe24bf
Jesús S.D <jesus.silla@miraiespana.com>
2013-06-07 12:00:32 +0200

    On submit go to new page and recive url vars

--- 04/Jsdiez/scripts/form-validate.js
+++ 04/Jsdiez/scripts/form-validate.js
@@ -21,12 +21,38 @@ formValidate = function(){
 			inputForm[i].className = 'correcto';
 		}		
 	}
-	formValidateMenssage();
-	return false;
+		formValidateMenssage();
+		return false;
 }
 
 //Lanzar mensaje con los campos que faltan por rellenar
 formValidateMenssage = function(){
-	alert('Por favor, rellene los campos '+inputNoValid);
-	inputNoValid = [];
+	if(inputNoValid == ""){
+		form.submit();
+	}else{
+		alert('Por favor, rellene los campos '+inputNoValid);
+		inputNoValid = [];
+	}
+}
+
+
+
+// Funcion para rescatar variables por url
+rescatarInfo = function(){
+	function getQueryVariable(variable){
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
+
+	var nombre = getQueryVariable("nombre")
+	var apellidos = getQueryVariable("apellidos");
+	var poblacion = getQueryVariable("poblacion");
+	
+	document.getElementById("poblacion").innerHTML=poblacion;
+	document.getElementById("nombre").innerHTML=nombre + " " + apellidos;	
 }
