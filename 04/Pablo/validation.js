78b2afa5e1f3b2f9b75a368ff7b3bb0383c33ba1
Pablo Barcia <pbarciam@gmail.com>
2013-06-07 12:18:18 +0200

    Create variable and add function #myForm

--- 04/Pablo/validation.js
+++ 04/Pablo/validation.js
@@ -9,6 +9,12 @@ Copyright: 2013 Pablo Barcia
 $(document).ready(function(){
 
 	// Colocamos los ID de los campos obligatorio aqui.
-	
+	var required = ["name", "lastname", "city"];
 	// Texto a mostrar para los campos incorrectos
-	
+	var emptyerror = "Por favor rellene este campo.";
+
+	$("#myForm").submit(function(){	
+
+
+				
+	});	
