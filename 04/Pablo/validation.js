b0ec0b45c1f48637dc34758df7588845a6a51fc7
Pablo Barcia <pbarciam@gmail.com>
2013-06-07 12:16:25 +0200

    Create file validation.js, answe.html
    File validate.js for validate inputs in the form

---
+++ 04/Pablo/validation.js
@@ -0,0 +1,14 @@
+/*
+Creado el 06/06/2013							
+Proyecto: yeswecan		
+Copyright: 2013 Pablo Barcia
+
+*/
+
+
+$(document).ready(function(){
+
+	// Colocamos los ID de los campos obligatorio aqui.
+	
+	// Texto a mostrar para los campos incorrectos
+	
